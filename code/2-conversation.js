78cc9007e9755073a206f248aa4a2d4290166003
Varuni <nadikuva57@craighead.school.nz>
2023-06-23 10:51:45 +1200

    Update 2-conversation.js

--- code/2-conversation.js
+++ code/2-conversation.js
@@ -5,14 +5,19 @@
 function conversation() {
   alert('Welcome to my conversation program.')
 
-  alert('Do you like cycling? Answer yes or no.')
-  let answer = prompt()
+  let answer = prompt('Do you like cycling? Answer yes or no.')
 
   if (answer == 'yes') {
     alert("That's good - you will get very fit.")
   } else {
     alert('Perhaps you like some other sport.')
+    let olympic = prompt('Do you watch Olympic athletics? Answer yes or no.')
   }
+  if (olympic == 'yes'){
+    alert("That's good - it's inspiring.")
+   } else {
+    alert("It's not for everyone.")
+   }
 
   alert('Goodbye')
 }
