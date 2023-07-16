e125b0a425a8897d6aa30366b6fc916d9094a0e9
Varuni <nadikuva57@craighead.school.nz>
2023-07-17 06:41:23 +1200

    Update

--- code/5-sale-calculator.js
+++ code/5-sale-calculator.js
@@ -3,5 +3,15 @@
 // ####################################
 
 function saleCalculator() {
-  // write your code here
+  let money = prompt("How much money do you have?")
+  let price = prompt("What is the ticket price of the item?")
+  let discount = prompt("What  is the discount percentage?")
+
+  let discountedPrice = price -(price*(discount/100))
+
+  if(money>=discountedPrice){
+    alert("You can afford this item.")
+  } else{
+    alert("Sorry you cannot afford this item.")
+  }
 }
